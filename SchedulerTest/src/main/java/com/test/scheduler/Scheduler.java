8acdb5fb437db3090031e051cdb32ae8cd5bfa2b
NyeongB <kdtoaj14@ajou.ac.kr>
2021-02-28 17:47:44 +0900

    cool sms library 추가

--- SchedulerTest/src/main/java/com/test/scheduler/Scheduler.java
+++ SchedulerTest/src/main/java/com/test/scheduler/Scheduler.java
@@ -3,20 +3,22 @@ package com.test.scheduler;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.test.sms.Send;
+
 @Component
 public class Scheduler
 {
 	/**
-	 * 1. 매월 1일 새벽 2시에 자동으로 결재완료하는 스케쥴러
+	 * 1. 매일 아침 8시 50분에 출근 후 QR 체크하라고 알림을 주는 프로그램
 	 */
-	// * 을 입력할경우 모두(항상)으로 설정함.
-	// 초 분 시 일 월 요일
 	
-
-	@Scheduled(cron = "0 37 17 * * *")
+	
+	@Scheduled(cron = "0 10 19 * * *")
 	public void autoUpdate()
 	{
-		System.out.println("오후 05:37:00에 호출이 됩니다 ");
+		String tel = "01099659257";
+		Send.send(tel);
+		//System.out.println("오후 05:37:00에 호출이 됩니다 ");
 	}
 	/*
 	 * 초 | 분 | 시 | 일 | 월 | 요일 | 연도
---
+++ SchedulerTest/src/main/java/com/test/sms/Send.java
@@ -0,0 +1,41 @@
+package com.test.sms;
+
+import java.util.HashMap;
+import org.json.simple.JSONObject;
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+
+// 휴대폰 인증을 위한 클래스
+public class Send
+{
+	public static void send(String tel)
+	{
+		// coolsms api 설정
+		String api_key = "NCSK3SWNJNK7KFNX";
+		String api_secret = "ASIQZOSSE7DOROFR4TZ1SOPX1ILC7UFR";
+		Message coolsms = new Message(api_key, api_secret);
+		
+		
+		// 문자 메세지 설정
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("to", tel); // 수신번호
+		params.put("from", "01099659257"); // 발신번호
+		params.put("type", "SMS"); // Message type ( SMS, LMS, MMS, ATA )
+		params.put("text", "COVID-19 QR 체크를 확인하세요. "); // 문자내용
+		params.put("app_version", "JAVA SDK v1.2"); // application name and version
+
+		try
+		{
+			// Json 형태로 보내기
+			JSONObject obj = (JSONObject) coolsms.send(params);
+			System.out.println(obj.toString());
+			
+		} catch (CoolsmsException e)
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCode());
+		}
+		
+		
+	}
+}
